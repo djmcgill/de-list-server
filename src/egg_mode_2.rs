c786232750ab83463ff0aed79115ea89c259fcf3
David McGillicuddy <contact@djmcgill.co.uk>
2019-04-07 20:12:43 +0100

    add redirect to twitter authenticate

--- src/egg_mode_2.rs
+++ src/egg_mode_2.rs
@@ -1,47 +1,35 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::error::Error;
-use std::iter::FromIterator;
+use std::fmt::Write;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use egg_mode::KeyPair;
-use futures::{future, Future};
+use futures::Future;
 use futures::stream::Stream;
 use hmac::{Hmac, Mac};
-use hyper::{Method, Request, Response, Uri};
+use hyper::{Method, Request, Uri};
 use hyper::body::Body;
 use hyper::client::{Client, HttpConnector};
-use hyper::header::{self, AUTHORIZATION, HeaderValue};
+use hyper::header::{AUTHORIZATION, HeaderValue};
 use hyper_tls::HttpsConnector;
 use rand::distributions::{Alphanumeric, Distribution};
 use sha1::Sha1;
 use url::percent_encoding::{EncodeSet, utf8_percent_encode};
 
 pub const REQUEST_TOKEN: &'static str = "https://api.twitter.com/oauth/request_token";
+pub const AUTHENTICATE: &'static str = "https://api.twitter.com/oauth/authenticate";
 
-///Convenience type used to hold parameters to an API call.
-pub type ParamList<'a> = HashMap<Cow<'a, str>, Cow<'a, str>>;
 
-///Convenience function to add a key/value parameter to a `ParamList`.
-pub fn add_param<'a, K, V>(list: &mut ParamList<'a>, key: K, value: V) -> Option<Cow<'a, str>>
-    where K: Into<Cow<'a, str>>,
-          V: Into<Cow<'a, str>>
-{
-    list.insert(key.into(), value.into())
-}
-
-fn percent_encode(src: &str) -> String {
-    utf8_percent_encode(src, TwitterEncodeSet).collect::<String>()
-}
 
 /// egg_mode hasn't been updated for hyper 0.12 yet
 pub fn request_token<'a, 'b, S: Into<String>>(con_token: &'a KeyPair, callback: S, client: &'b Client<HttpsConnector<HttpConnector>, Body>)
-                                      -> impl Future<Item=(), Error=Box<(dyn std::error::Error + Send + Sync +'b)>> {
-//
-//    let header = get_header(Method::POST, REQUEST_TOKEN,
-//                            con_token, None, Some(callback.into()), None, None);
+                                      -> impl Future<Item=KeyPair, Error=Box<(dyn std::error::Error + Send + Sync +'b)>> {
+    let header = get_header(Method::POST, REQUEST_TOKEN,
+                            con_token, None, Some(callback.into()), None, None);
+    let header_value = header.header_value().unwrap();
     let request = Request::connect::<Uri>(REQUEST_TOKEN.parse().unwrap())
-        .header(AUTHORIZATION, HeaderValue::from_str("foo").unwrap())
+        .header(AUTHORIZATION, HeaderValue::from_str(&header_value).unwrap())
         .method(Method::POST)
         .body(Body::empty()).unwrap();
 
@@ -56,42 +44,30 @@ pub fn request_token<'a, 'b, S: Into<String>>(con_token: &'a KeyPair, callback: 
                 .map_err(|e| -> Box<dyn std::error::Error + Send + Sync> { Box::new(e) })
                 .map(|body| {
                     let s = String::from_utf8(body.into_iter().collect()).unwrap();
-                    println!("RESPONSE: {}", s);
-//                    let ret: KeyPair = panic!();
-//                    ret
-                    ()
+                    parse_tok(s).unwrap()
                 })
         })
+}
 
 
-//    future::err(panic!())
+fn parse_tok(full_resp: String) -> Result<KeyPair, Box<(dyn std::error::Error + Send + Sync)>> {
+    let mut key: Option<String> = None;
+    let mut secret: Option<String> = None;
 
+    for elem in full_resp.split('&') {
+        let mut kv = elem.splitn(2, '=');
+        match kv.next() {
+            Some("oauth_token") => key = kv.next().map(|s| s.to_string()),
+            Some("oauth_token_secret") => secret = kv.next().map(|s| s.to_string()),
+            Some(_) => (),
+            None => panic!(),
+        }
+    }
 
-//
-//    fn parse_tok(full_resp: String, _: &Headers) -> Result<KeyPair, error::Error> {
-//        let mut key: Option<String> = None;
-//        let mut secret: Option<String> = None;
-//
-//        for elem in full_resp.split('&') {
-//            let mut kv = elem.splitn(2, '=');
-//            match kv.next() {
-//                Some("oauth_token") => key = kv.next().map(|s| s.to_string()),
-//                Some("oauth_token_secret") => secret = kv.next().map(|s| s.to_string()),
-//                Some(_) => (),
-//                None =>
-//                    return Err(
-//                        error::Error::InvalidResponse(
-//                            "unexpected end of request_token response", None
-//                        )
-//                    ),
-//            }
-//        }
-//
-//        Ok(KeyPair::new(try!(key.ok_or(error::Error::MissingValue("oauth_token"))),
-//                        try!(secret.ok_or(error::Error::MissingValue("oauth_token_secret")))))
+    Ok(KeyPair::new(key.unwrap(), secret.unwrap()))
+}
 // make_future(handle, request, parse_tok)
 
-}
 
 ///With the given method parameters, return a signed OAuth header.
 fn get_header(method: Method,
@@ -132,6 +108,21 @@ impl EncodeSet for TwitterEncodeSet {
     }
 }
 
+///Convenience type used to hold parameters to an API call.
+pub type ParamList<'a> = HashMap<Cow<'a, str>, Cow<'a, str>>;
+
+///Convenience function to add a key/value parameter to a `ParamList`.
+pub fn add_param<'a, K, V>(list: &mut ParamList<'a>, key: K, value: V) -> Option<Cow<'a, str>>
+    where K: Into<Cow<'a, str>>,
+          V: Into<Cow<'a, str>>
+{
+    list.insert(key.into(), value.into())
+}
+
+fn percent_encode(src: &str) -> String {
+    utf8_percent_encode(src, TwitterEncodeSet).collect::<String>()
+}
+
 ///With the given OAuth header and method parameters, create an OAuth signature and return the
 ///header with the signature inline.
 fn sign(header: TwitterOAuth,
@@ -140,58 +131,55 @@ fn sign(header: TwitterOAuth,
         params: Option<&ParamList>,
         con_token: &KeyPair,
         access_token: Option<&KeyPair>) -> TwitterOAuth {
-//    let query_string = {
-//        let mut sig_params = params.cloned().unwrap_or_default();
-//
-//        add_param(&mut sig_params, "oauth_consumer_key", header.consumer_key.as_str());
-//        add_param(&mut sig_params, "oauth_nonce", header.nonce.as_str());
-//        add_param(&mut sig_params, "oauth_signature_method", "HMAC-SHA1");
-//        add_param(&mut sig_params, "oauth_timestamp", format!("{}", header.timestamp));
-//        add_param(&mut sig_params, "oauth_version", "1.0");
-//
-//        if let Some(ref token) = header.token {
-//            add_param(&mut sig_params, "oauth_token", token.as_str());
-//        }
-//
-//        if let Some(ref callback) = header.callback {
-//            add_param(&mut sig_params, "oauth_callback", callback.as_str());
-//        }
-//
-//        if let Some(ref verifier) = header.verifier {
-//            add_param(&mut sig_params, "oauth_verifier", verifier.as_str());
-//        }
-//
-//        let mut query = sig_params.iter()
-//            .map(|(k, v)| format!("{}={}", percent_encode(k), percent_encode(v)))
-//            .collect::<Vec<_>>();
-//        query.sort();
-//
-//        query.join("&")
-//    };
-//
-//    let base_str = format!("{}&{}&{}",
-//                           percent_encode(method.as_ref()),
-//                           percent_encode(uri),
-//                           percent_encode(&query_string));
-//    let key = format!("{}&{}",
-//                      percent_encode(&con_token.secret),
-//                      percent_encode(&access_token.unwrap_or(&KeyPair::new("", "")).secret));
-//
-//    let mut digest = Hmac::<Sha1>::new(key.as_bytes());
-//    digest.input(base_str.as_bytes());
-//
-//    let config = base64::Config {
-//        char_set: base64::CharacterSet::Standard,
-//        newline: base64::Newline::LF,
-//        pad: true,
-//        line_length: None,
-//    };
-//
-//    TwitterOAuth {
-//        signature: Some(digest.result().code().to_base64(config)),
-//        ..header
-//    }
-    panic!()
+    let query_string = {
+        let mut sig_params = params.cloned().unwrap_or_default();
+
+        add_param(&mut sig_params, "oauth_consumer_key", header.consumer_key.as_str());
+        add_param(&mut sig_params, "oauth_nonce", header.nonce.as_str());
+        add_param(&mut sig_params, "oauth_signature_method", "HMAC-SHA1");
+        add_param(&mut sig_params, "oauth_timestamp", format!("{}", header.timestamp));
+        add_param(&mut sig_params, "oauth_version", "1.0");
+
+        if let Some(ref token) = header.token {
+            add_param(&mut sig_params, "oauth_token", token.as_str());
+        }
+
+        if let Some(ref callback) = header.callback {
+            add_param(&mut sig_params, "oauth_callback", callback.as_str());
+        }
+
+        if let Some(ref verifier) = header.verifier {
+            add_param(&mut sig_params, "oauth_verifier", verifier.as_str());
+        }
+
+        let mut query = sig_params.iter()
+            .map(|(k, v)| format!("{}={}", percent_encode(k), percent_encode(v)))
+            .collect::<Vec<_>>();
+        query.sort();
+
+        query.join("&")
+    };
+
+    let base_str = format!("{}&{}&{}",
+                           percent_encode(method.as_ref()),
+                           percent_encode(uri),
+                           percent_encode(&query_string));
+    let key = format!("{}&{}",
+                      percent_encode(&con_token.secret),
+                      percent_encode(&access_token.unwrap_or(&KeyPair::new("", "")).secret));
+
+    let mut digest = Hmac::<Sha1>::new_varkey(key.as_bytes()).unwrap();
+    digest.input(base_str.as_bytes());
+
+    let config = base64::Config::new(
+        base64::CharacterSet::Standard,
+        true,
+    );
+
+    TwitterOAuth {
+        signature: Some(base64::encode_config(&digest.result().code(), config)),
+        ..header
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -249,40 +237,35 @@ impl std::str::FromStr for TwitterOAuth {
 }
 
 impl TwitterOAuth {
-    fn fmt_scheme(&self) -> String { panic!() }
+    fn header_value(&self) -> Result<String, std::fmt::Error> {
+        let mut ret = String::new();
+        write!(ret, "OAuth ")?;
+
+        write!(ret, "oauth_consumer_key=\"{}\"", percent_encode(&self.consumer_key))?;
+
+        write!(ret, ", oauth_nonce=\"{}\"", percent_encode(&self.nonce))?;
+
+        if let Some(ref signature) = self.signature {
+            write!(ret, ", oauth_signature=\"{}\"", percent_encode(signature))?;
+        }
+
+        write!(ret, ", oauth_signature_method=\"{}\"", percent_encode("HMAC-SHA1"))?;
+
+        write!(ret, ", oauth_timestamp=\"{}\"", self.timestamp)?;
+
+        if let Some(ref token) = self.token {
+            write!(ret, ", oauth_token=\"{}\"", percent_encode(token))?;
+        }
+
+        write!(ret, ", oauth_version=\"{}\"", "1.0")?;
+
+        if let Some(ref callback) = self.callback {
+            write!(ret, ", oauth_callback=\"{}\"", percent_encode(callback))?;
+        }
+
+        if let Some(ref verifier) = self.verifier {
+            write!(ret, ", oauth_verifier=\"{}\"", percent_encode(verifier))?;
+        }
+        Ok(ret)
+    }
 }
-//impl Scheme for TwitterOAuth {
-//    fn scheme() -> Option<&'static str> {
-//        Some("OAuth")
-//    }
-//
-//    fn fmt_scheme(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//        write!(f, "oauth_consumer_key=\"{}\"", percent_encode(&self.consumer_key))?;
-//
-//        write!(f, ", oauth_nonce=\"{}\"", percent_encode(&self.nonce))?;
-//
-//        if let Some(ref signature) = self.signature {
-//            write!(f, ", oauth_signature=\"{}\"", percent_encode(signature))?;
-//        }
-//
-//        write!(f, ", oauth_signature_method=\"{}\"", percent_encode("HMAC-SHA1"))?;
-//
-//        write!(f, ", oauth_timestamp=\"{}\"", self.timestamp)?;
-//
-//        if let Some(ref token) = self.token {
-//            (write!(f, ", oauth_token=\"{}\"", percent_encode(token))?;
-//        }
-//
-//        write!(f, ", oauth_version=\"{}\"", "1.0")?;
-//
-//        if let Some(ref callback) = self.callback {
-//            write!(f, ", oauth_callback=\"{}\"", percent_encode(callback))?;
-//        }
-//
-//        if let Some(ref verifier) = self.verifier {
-//            write!(f, ", oauth_verifier=\"{}\"", percent_encode(verifier))?;
-//        }
-//
-//        Ok(())
-//    }
-//}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use tera::{compile_templates, Context, Tera};
+use tera::{compile_templates, Context, Tera, Value};
 use lazy_static::lazy_static;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::rt::{self, Future};
 use hyper::service::service_fn;
-use futures::future;
 use std::env;
 use egg_mode;
-use env_logger;
-use log::{trace, LevelFilter};
-use tokio_core::reactor::{Core, Handle};
-use env_logger::Builder;
 use egg_mode::KeyPair;
 use hyper_tls::HttpsConnector;
 use hyper::client::{Client, HttpConnector};
@@ -22,52 +17,51 @@ lazy_static! {
         tera.autoescape_on(vec!["html"]);
         tera
     };
+
+    pub static ref CLIENT_POOL: Client<HttpsConnector<HttpConnector>> = {
+        let https = HttpsConnector::new(4).unwrap();
+        let client = Client::builder()
+            .build::<_, hyper::Body>(https);
+        client
+    };
+
+    pub static ref CONSUMER_TOKEN: KeyPair = {
+        let consumer_key = env::var("CONSUMER_KEY").unwrap();
+        let consumer_secret = env::var("CONSUMER_SECRET").unwrap();
+        KeyPair::new(consumer_key, consumer_secret)
+    };
 }
 
 const CALLBACK_URL: &'static str = "http://localhost:3000/sign-in-with-twitter";
 
-//type BoxFut = Box<Future<Item=Response<Body>, Error=Box<std::error::Error>> + Send>;
-//fn hello_world(_req: Request<Body>, consumer_token: &KeyPair, client: &Client<HttpsConnector<HttpConnector>, Body>) -> BoxFut {
-//
-//
-////    let mut runtime = Core::new().unwrap(); // ahhh this is really bad
-////
-////    let req_token = egg_mode::request_token(
-////        consumer_token,
-////        CALLBACK_URL,
-////        handle).and_then(|res| {
-////        trace!("{:?}", req_token);
-////
-////    });
-//
-//    // I know I need to move this into the and_then, but I can't even get the above to compile
-////    Box::new(future::ok(R))
-//}
+fn redirect_to_twitter_authenticate(_req: Request<Body>) -> impl Future<Item=Response<Body>, Error=Box<(dyn std::error::Error + Send + Sync + 'static)>> {
+    let key_pair_future = egg_mode_2::request_token(&CONSUMER_TOKEN, CALLBACK_URL, &CLIENT_POOL);
+    Box::new(key_pair_future.map(|oauth_token| {
+        let redirect_url = format!("{}?oauth_token={}", egg_mode_2::AUTHENTICATE, oauth_token.key);
+
+        let mut context = Context::new();
+        context.insert("redirect_url", &Value::String(redirect_url.clone()));
+
+        let mut response = Response::new(Body::from(TERA.render("redirect.html", &context).unwrap()));
+        *response.status_mut() = StatusCode::from_u16(302).unwrap();
+        response.headers_mut().insert(
+            hyper::header::LOCATION,
+            hyper::header::HeaderValue::from_str(&redirect_url).unwrap()
+        );
+        response
+    }))
+}
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let mut builder = Builder::new();
-    builder.filter_module("main", LevelFilter::Trace).init();
-
-    let consumer_key = env::var("CONSUMER_KEY")?;
-    let consumer_secret = env::var("CONSUMER_SECRET")?;
-    let consumer_token = KeyPair::new(consumer_key, consumer_secret);
+    env_logger::init();
 
     let mut core = Core::new().unwrap();
 
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let https = HttpsConnector::new(4).unwrap();
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
-
     let server = Server::bind(&addr)
         .serve(|| {
-            service_fn(|req| {
-                let key_pair_future = egg_mode_2::request_token(&consumer_token, CALLBACK_URL, &client);
-                Box::new(key_pair_future.map(|_|
-                    Response::new(Body::from(TERA.render("hello.html", &Context::new()).unwrap()))
-                ))
-            })
+            service_fn(redirect_to_twitter_authenticate)
         })
         .map_err(|e| {eprintln!("server error: {}", e); e});
 
